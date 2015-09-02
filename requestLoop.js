353e3d6196ad5cbe565913980743f3b349683350
unknown <kso@LPCP1077.BEUMER.com>
2015-09-02 10:54:39 +0200

    added element name as a parameter

--- requestLoop.js
+++ requestLoop.js
@@ -2,13 +2,19 @@ var request = require('request');
 
 var counter = 0;
 
+if (process.argv.length != 3) {
+  console.log('please specify element name');
+  return;
+}
+var elementName = process.argv[2];
+
 enterRequestLoop(counter);
 
 function enterRequestLoop(count) {
 request({
 url: 'http://178.62.223.30:8086/write?db=spongy',
 method: 'POST',
-body: 'tracking,element=gert value=1'
+body: 'tracking,element=elementName value=1'
 }, function(error,response,body) {
 if (error) {
   console.log(error);
