0a084275baccbc612fdf891423b7f9fc7126240f
unknown <kso@LPCP1077.BEUMER.com>
2015-09-02 12:14:37 +0200

    remove debug

--- processFiles.sh
+++ processFiles.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 FILES=x*
-echo ${#FILES[@]}
-#for f in $FILES
-#do
-#  echo "Processing $f file..."
-#  nodejs parseDataStream.js $f
-#done
+for f in $FILES
+do
+  echo "Processing $f file..."
+  nodejs parseDataStream.js $f
+done
