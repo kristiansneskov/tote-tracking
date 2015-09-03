b211e36ffc3bd8045fc513edca8fe075b701f3c1
unknown <kso@LPCP1077.BEUMER.com>
2015-09-03 12:39:54 +0200

    updated bulk processing of files via bash to use curl

--- processFiles.sh
+++ processFiles.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 FILES=x*
+COUNT=0
 for f in $FILES
 do
-  echo "Processing $f file..."
-  nodejs parseDataStream.js $f
+  echo "Processing $f file number $COUNT..." 
+  curl -X POST 'http://localhost:8086/write?db=spongy' --data-binary @$f
+#  nodejs parseDataStream.js $f
+  COUNT = $((COUNT+1))
 done
