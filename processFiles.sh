15412667ecc3ba982fd0164902ed30a04efae1b8
unknown <kso@LPCP1077.BEUMER.com>
2015-09-04 10:02:21 +0200

    inserted sleep to give influx a breather

--- processFiles.sh
+++ processFiles.sh
@@ -5,6 +5,7 @@ COUNT=0
 for f in $FILES
 do
   echo "Processing $f file number $COUNT..." 
+  sleep 10
   curl -X POST 'http://localhost:8086/write?db=crisplant' --data-binary @$f
 #  nodejs parseDataStream.js $f
   COUNT = $((COUNT+1))
