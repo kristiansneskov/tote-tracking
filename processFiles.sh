cafd10f2fed8969a1257c1a23f5dfa525e76d707
unknown <kso@LPCP1077.BEUMER.com>
2015-09-04 09:00:27 +0200

    Update process script

--- processFiles.sh
+++ processFiles.sh
@@ -5,7 +5,7 @@ COUNT=0
 for f in $FILES
 do
   echo "Processing $f file number $COUNT..." 
-  curl -X POST 'http://localhost:8086/write?db=spongy' --data-binary @$f
+  curl -X POST 'http://localhost:8086/write?db=crisplant' --data-binary @$f
 #  nodejs parseDataStream.js $f
   COUNT = $((COUNT+1))
 done
