3cd4831bd5e3e5cf8bf6e4f982571165c5d369a3
unknown <kso@LPCP1077.BEUMER.com>
2015-09-04 10:21:02 +0200

    Increase curl timeout

--- processFiles.sh
+++ processFiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-FILES=xab*
+FILES=x*
 COUNT=0
 for f in $FILES
 do
   echo "Processing $f file number $COUNT..." 
-  sleep 10
+  sleep 30
   curl -X POST 'http://localhost:8086/write?db=crisplant' --data-binary @$f
 #  nodejs parseDataStream.js $f
   ((COUNT=COUNT+1))
